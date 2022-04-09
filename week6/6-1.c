f611b06ab569e380161e8e5897d06ec70c2e837c
Junyong Moon <jymoon1121@gmail.com>
2022-04-10 02:26:51 +0900

    create-week6-contents

---
+++ week6/6-1.c
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
+// 2018117610 moonjunyong
+
+#include <stdio.h>
+#define MAX 17
+#define INF 100000
+
+// Functions
+int minmult(int n, int d[], int P[][MAX], int M[][MAX]);
+int minimum(int i, int j, int *minK, int M[][MAX], int d[MAX]);
+void order(int i, int j, int P[][MAX]);
+
+
+
+// Global variable
+int P[MAX][MAX] = { 0, };
+int M[MAX][MAX] = { 0, };
+int arrD[MAX] = { 0, };
+
+int optimum;
+
+
+// Main
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	for (int i = 0; i < num + 1; i++) {
+		scanf("%d", &arrD[i]);
+	}
+	minmult(num, arrD, P, M);
+	
+	// M
+	for (int i = 1; i < num; i++) {
+		printf("0 ");
+		for (int j = i+1; j <= num; j++) {
+			printf("%d", M[i][j]);
+			if (j < num) {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	printf("0\n");
+
+	// P
+	for (int i = 1; i < num; i++) {
+		printf("0 ");
+		for (int j = i+1; j <= num; j++) {
+			printf("%d", P[i][j]);
+			if (j < num) {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	printf("0\n");
+
+	printf("%d\n", optimum);
+	order(1, num, P);
+}
+
+int minmult(int n, int d[], int P[][MAX], int M[][MAX]) 
+{
+	int i, j, K, diagonal;
+	for (i = 0; i <= n; i++)
+		M[i][i] = 0;
+	for (diagonal = 1; diagonal <= n - 1; diagonal++)
+		for (i = 1; i <= n - diagonal; i++) {
+			j = i + diagonal;
+			M[i][j] = minimum(i, j, &K, M, d);
+			P[i][j] = K;
+		}
+	return M[1][n];
+}
+
+
+int minimum(int i, int j, int *minK, int M[][MAX], int d[MAX]) 
+{
+	int minV = INF;
+	for (int k = i; k <= j - 1; k++) {
+		int value = M[i][k] + M[k + 1][j] + d[i - 1] * d[k] * d[j];
+		if (minV > value) {
+			minV = value;
+			*minK = k;
+		}
+	}
+	optimum = minV;
+	return minV;
+}
+
+void order(int i, int j, int P[][MAX]) 
+{
+	if (i == j)
+		printf("(A%d)", i);
+	else {
+		int k = P[i][j];
+		printf("(");
+		order(i, k, P);
+		order(k + 1, j, P);
+		printf(")");
+	}
+}
---
+++ week6/6-2.c
@@ -0,0 +1,174 @@
+#define _CRT_SECURE_NO_WARNINGS
+// 2018117610 moonjunyong
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 20
+#define INF 9999999
+
+
+// Data structure
+typedef struct node* node_pointer;
+typedef struct node {
+	int key;
+	node_pointer left;
+	node_pointer right;
+} nodetype;
+
+
+// Global variable
+int A[MAX][MAX] = { 0, };
+int R[MAX][MAX] = { 0, };
+int p[MAX];
+
+int optsearchtreeV = 0;
+int minV = 0;
+int optimum = 0; // optsearchtreeV + minV
+
+
+// Functions
+void optsearchtree(int n, int arrp[], int R[][MAX]);
+int minimum(int i, int j, int* minK, int A[][MAX]);
+node_pointer tree(int i, int j, int R[][MAX], int keys[MAX]);
+void inorder(node_pointer p);
+void preorder(node_pointer p);
+
+
+// Main
+int main()
+{
+	node_pointer ptk;
+	int num;
+	scanf("%d", &num);
+	int arrK[MAX] = { 0, };
+	for (int i = 1; i <= num; i++) {
+		scanf("%d", &arrK[i]);
+	}
+	for (int i = 1; i <= num; i++) {
+		scanf("%d", &p[i]);
+	}
+
+	optsearchtree(num, p, R);
+
+    // First print the upper triangle of matrix A
+	for (int i = 1; i <= num; i++) {
+        printf("0 ");
+		for (int j = i; j <= num; j++) {
+			printf("%d", A[i][j]);
+			if (j < num) {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+    printf("0\n");
+
+    // Next, print the upper triangle of the matrix R. (including 0)
+	for (int i = 1; i <= num; i++) {
+        printf("0 ");
+		for (int j = i; j <= num; j++) {
+			printf("%d", R[i][j]);
+			if (j < num) {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+    printf("0\n");
+
+    // After outputting A and R, output the optimal value of the average search time in the optimal binary search tree.
+    // printf("optsearchtreeV : %d\t minv: %d\n", optsearchtreeV, minV);
+    optimum = optsearchtreeV + minV;
+    printf("%d\n", optimum);
+
+	ptk = tree(1, num, R, arrK);
+	preorder(ptk);      // print preorder
+	printf("\n");
+	inorder(ptk);       // print inorder
+}
+
+void optsearchtree(int n, int arrp[], int R[][MAX]) 
+{
+	int i, j, k, diagonal;
+	int sum = 0;
+	for (i = 1; i <= n; i++) {
+		A[i][i] = arrp[i]; A[i][i - 1] = 0;
+		R[i][i] = i; R[i][i - 1] = 0;
+	}
+	A[n + 1][n] = 0;
+	R[n + 1][n] = 0;
+	for (diagonal = 1; diagonal <= n - 1; diagonal++) {
+		for (i = 1; i <= n - diagonal; i++) {
+			sum = 0;
+			j = i + diagonal;
+				for (int m = i; m <= j; m++) {
+					sum += arrp[m];
+				}
+
+			A[i][j] = minimum(i, j, &k, A) + sum;
+			R[i][j] = k;
+		}
+    optsearchtreeV = sum;
+    }
+}
+
+int minimum(int i, int j, int* minK, int A[][MAX]) 
+{
+    minV = INF;
+	for (int k = i; k <= j; k++) {
+		int value = A[i][k-1] + A[k + 1][j];
+		if (minV > value) {
+			minV = value;
+			*minK = k;
+		}
+	}
+	return minV;
+}
+
+node_pointer tree(int i, int j, int R[][MAX], int keys[MAX]) 
+{
+	int k;
+	node_pointer p;
+	k = R[i][j];
+	if (k == 0)
+		return NULL;
+	else {
+		p = (node_pointer)malloc(sizeof(nodetype));
+		p->key = keys[k];
+		p->left = tree(i, k - 1, R, keys);
+		p->right = tree(k + 1, j, R, keys);
+		return p;
+	}
+}
+
+void inorder(node_pointer p) 
+{
+	if (p) {
+		if (p->left) {
+			inorder(p->left);
+			printf(" ");
+		}
+		printf("%d", p->key);
+		if (p->right) {
+			printf(" ");
+			inorder(p->right);
+		}
+	}
+}
+
+void preorder(node_pointer p) 
+{
+	if (p) {
+		printf("%d", p->key);
+		if (p->left) {
+			printf(" ");
+			preorder(p->left);
+		}
+		if (p->right) {
+			printf(" ");
+			preorder(p->right);
+		}
+	}
+}
