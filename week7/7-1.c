e6fd84fa8ff527c5a400163f60fc62c65640548e
Junyong Moon <jymoon1121@gmail.com>
2022-04-14 22:07:41 +0900

    Create W7_7-1.c

---
+++ week7/7-1.c
@@ -0,0 +1,104 @@
+// 2018117610 moonjunyong
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define INF 1024
+#define MAX 128
+
+
+// Global variable
+int G[MAX][MAX], n, F[MAX][MAX];
+
+
+// Function
+void prims();
+
+
+// Main
+int main(){
+    int i, j, total_cost, m;
+    scanf("%d %d", &n, &m);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j <= n; j++) {
+            G[i][j] = 0;
+        }
+    }
+
+    int num1, num2, num3;
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d %d %d", &num1, &num2, &num3);
+        G[num1 - 1][num2 - 1] = num3;
+        G[num2 - 1][num1 - 1] = num3;
+    }
+    //printf("1 1 1 1\n");
+    prims();
+
+    for (i = 0; i < n - 1; i++) {
+        printf("%d %d %d\n", F[i][0], F[i][1], F[i][2]);
+    }
+    return 0;
+}
+
+
+// Function
+void prims()
+{
+    int arr[MAX][MAX];
+    int u, v, min, distance[MAX], nearest[MAX];
+    int temp[MAX], i, j;
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+        {
+            if (G[i][j] == 0)
+                arr[i][j] = INF;
+            else
+                arr[i][j] = G[i][j];
+        }
+    distance[0] = 0;
+    temp[0] = 1;
+
+    for (i = 1; i < n; i++)
+    {
+        distance[i] = arr[0][i];
+        nearest[i] = 1;
+        temp[i] = 0;
+    }
+
+    for (int z = 0; z < n - 1; z++)
+    {
+        min = INF;
+        for (i = 1; i < n; i++)
+            if (temp[i] == 0 && distance[i] < min)
+            {
+                v = i;
+                min = distance[i];
+            }
+
+        u = nearest[v];
+        
+        F[z][0] = v+1;
+        F[z][1] = u;
+        F[z][2] = G[v][u-1];
+
+        temp[v] = 1;
+
+        for (i = 1; i < n; i++)
+            if (temp[i] == 0 && arr[i][v] < distance[i])
+            {
+                distance[i] = arr[i][v];
+                nearest[i] = v + 1;
+            }
+        distance[v] = -1;
+
+        for (i = 1; i < n - 1; i++) {
+            printf("%d ", nearest[i]);
+        }
+        printf("%d", nearest[n - 1]);
+        printf("\n");
+
+    }
+}
