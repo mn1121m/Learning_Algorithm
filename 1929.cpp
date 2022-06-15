d094cd1f934b652c83f88926aa602b7c34a274ab
junyong <jymoon1121@gmail.com>
2022-06-15 09:05:18 +0900

    Create 1929.cpp

---
+++ 1929.cpp
@@ -0,0 +1,26 @@
+// BOJ # 1929. 소수 구하기 (에라토스테네스의 체)
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int min,max;
+    bool *prime;
+    cin >> min;
+    cin >> max;
+    prime = new bool[max+1];
+    fill_n(prime, max+1, 1);
+    prime[0] = false;
+    prime[1] = false;
+
+    for (int i = 2; i <= sqrt(max); i++)
+        if(prime[i] == true)
+            for(int j = i*2; j <= max; j += i)
+                prime[j] = false;
+
+    for (int i = min; i <= max; i++)
+        if(prime[i] == true)
+            cout << i << "\n";
+   return 0;
+}
