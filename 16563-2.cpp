68602ac5ae90cc36e307f091daebe5eaeb8846d9
junyong <jymoon1121@gmail.com>
2022-06-27 18:42:10 +0900

    Create 16563-2.cpp

---
+++ 16563-2.cpp
@@ -0,0 +1,53 @@
+// BOJ # 16563. 어려운 소인수분해 (에라토스테네스의 체 응용
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#define endl '\n'
+#define MAX 1234
+
+using namespace std;
+
+int n;
+int minFactor[MAX+1];
+
+void updateFactor(){
+  for (int i=2; i<=MAX; i++)
+    minFactor[i] = i;
+  
+  for (int i=2; i<=sqrt(MAX); i++)
+    if (minFactor[i] == i)
+      for (int j=i*i; j<=MAX; j+=i)
+        if (minFactor[j] == j)
+          minFactor[j] = i;
+}
+
+void solve(int num){
+  while (num > 1){
+    cout << minFactor[num] << " ";
+    num /= minFactor[num];
+  }
+  cout << endl;
+}
+
+int main(){
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  // ifstream cin;
+  // cin.open("input.txt");
+
+  cin >> n;
+
+  updateFactor();
+
+  for (int i=0; i<n; i++){
+    int num;
+    cin >> num;
+    solve(num);
+  }
+
+}
